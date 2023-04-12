95d9c70d373733e71dbfd3f1e1560104a0e8ce68
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-11 20:29:31 -0400

    Implement Header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,51 @@
+import { Component } from "solid-js"
+import { A, useLocation } from "@solidjs/router"
+import Logo from "../global/Logo"
+
+const Header: Component<{}> = (props) => {
+  const location = useLocation()
+  const active = (path: string) =>
+    path == location.pathname ? "underline" : ""
+  return (
+    <header class="flex flex-row justify-between align-center w-full py-8 px-[10.25rem] fixed">
+      {/* Links */}
+      <nav class="flex flex-row items-center  gap-8 h-16">
+        <Logo />
+        <div class="bg-dark-blue w-px h-full" />
+        <ul class="flex flex-row gap-7 text-xs">
+          <li>
+            <A href="/" class={active("/")}>
+              HOME
+            </A>
+          </li>
+          <li>
+            <A href="/about" class={active("/about")}>
+              ABOUT
+            </A>
+          </li>
+          <li>
+            <A href="/pricing" class={active("/pricing")}>
+              PRICING
+            </A>
+          </li>
+          <li>
+            <A href="/tokens" class={active("/tokens")}>
+              TOKENS
+            </A>
+          </li>
+          <li>
+            <A href="/contact" class={active("/contact")}>
+              CONTACT US
+            </A>
+          </li>
+        </ul>
+      </nav>
+      {/* Download App Button */}
+      <button class="bg-blue rounded-[5rem] py-5 px-8 flex flex-row justify-center items-center gap-1 w-52 h-14 leading-4">
+        Download App
+      </button>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/root.tsx
@@ -0,0 +1,41 @@
+// @refresh reload
+import { Suspense } from "solid-js"
+import {
+  useLocation,
+  A,
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title,
+} from "solid-start"
+import "./root.css"
+import Header from "./components/layout/Header"
+
+export default function Root() {
+  return (
+    <Html lang="en" class="bg-bg">
+      <Head>
+        <Title>FinanceFlow</Title>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>
+        <Suspense>
+          <ErrorBoundary>
+            <Header />
+
+            <Routes>
+              <FileRoutes />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
+        <Scripts />
+      </Body>
+    </Html>
+  )
+}
