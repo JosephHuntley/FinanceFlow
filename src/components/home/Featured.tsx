1c8d6d812e9cd6162de312157935c50f93fe1331
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-13 21:32:57 -0400

    Responsive Design

--- src/components/home/Featured.tsx
+++ src/components/home/Featured.tsx
@@ -3,14 +3,20 @@ import RandLogo from "/CompanyLogo.png"
 
 const Featured: Component<{}> = (props) => {
   return (
-    <section class="flex flex-col items-center gap-8">
+    <section class="flex flex-col items-center gap-8 sm:mt-0 mt-16">
       <h2 class="text-lg font-normal">Finance Flow has been featured on</h2>
-      <div class="flex flex-row gap-16">
-        <img src={RandLogo} />
-        <img src={RandLogo} />
-        <img src={RandLogo} />
-        <img src={RandLogo} />
-        <img src={RandLogo} />
+      <div class="flex sm:flex-row flex-col sm:gap-16 gap-6">
+        <div class="flex flex-row justify-center">
+          <img src={RandLogo} />
+          <img src={RandLogo} />
+        </div>
+        <div class="flex flex-row justify-center">
+          <img src={RandLogo} />
+          <img src={RandLogo} />
+        </div>
+        <div class="flex flex-row justify-center">
+          <img src={RandLogo} />
+        </div>
       </div>
     </section>
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       {/* Adding mt-32 instead of pt-32 causes issue with fixed header */}
-      <main class="pt-32 mx-40 min-h-[100vh]">
+      <main class="pt-32 sm:mx-40 mx-4 min-h-[100vh]">
         <Hero />
         <Featured />
       </main>
