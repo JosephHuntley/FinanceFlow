71a4580161af63cb38701b1b0a74171c1eef107b
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-14 13:06:07 -0400

    Improved SEO by added description

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Head, Title, Meta } from "solid-start"
 import Featured from "~/components/home/Featured"
 import Hero from "~/components/home/Hero"
 import Portfolio from "~/components/home/Portfolio"
@@ -6,6 +7,12 @@ import Rewards from "~/components/home/Rewards"
 export default function Home() {
   return (
     <>
+      <Head>
+        <Title>FinanceFlow</Title>
+        <Meta charset="utf-8" />
+        <Meta name="description" content="Home page of Finance Flow" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       {/* Adding mt-32 instead of pt-32 causes issue with fixed header */}
       <main class="pt-32 sm:mx-40 mx-4 min-h-[100vh]">
         <Hero />
