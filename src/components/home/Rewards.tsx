539f6624c28ac2235bb14b2b1d0207443920cf53
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-21 15:54:44 -0400

    Responsive Design

--- src/components/home/Rewards.tsx
+++ src/components/home/Rewards.tsx
@@ -1,22 +1,34 @@
 import { Component } from "solid-js"
 import DualPhone from "/DualPhone.png"
+import { rewards } from "../../data/Constants"
+import Laptop from "/Computer.png"
 
 const Rewards: Component<{}> = (props) => {
   return (
-    <section class="flex flex-coł mt-20">
+    <section class="grid   my-20 grid-cols-1 sm:grid-cols-2">
       {/* Dual Phone */}
-      <div class="flex sm:flex-row flex-col">
-        <img class="w-full h-[380px]" src={DualPhone} />
-        <h2 class="text-xl font-bold w-[304px] mt-10">
-          Earn daily rewards on your idle tokens
-        </h2>
-        <p class="mt-4 max-w-[328px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nulla
-          suspendisse tortor aene.
-        </p>
-      </div>
+      {/* <div class="flex sm:flex-row flex-col"> */}
+      <img class="w-full h-[380px]" src={DualPhone} />
+
+      {rewards.map((reward) => (
+        <div class="flex flex-col my-10">
+          <h2 class="text-3xl font-bold ">{reward.title}</h2>
+          <p class="mt-4 max-w-[328px]">{reward.text}</p>
+          {reward.bullets.map((bullet) => (
+            <>
+              <div class="flex mt-4 gap-4 items-center">
+                <img src={bullet.icon} />
+                <p>{bullet.text}</p>
+              </div>
+            </>
+          ))}
+        </div>
+      ))}
+      {/* </div> */}
       {/* Laptop */}
-      <div></div>
+      <div>
+        <img src={Laptop} />
+      </div>
     </section>
   )
 }
---
+++ src/data/Constants.ts
@@ -0,0 +1,45 @@
+import StockIcon from "/Stocks.png"
+import LightningIcon from "/Lightning.png"
+import LockIcon from "/Lock.png"
+import CheckMarkIcon from "/CheckMark.png"
+import UptimeIcon from "/Uptime.png"
+import MessageIcon from "/Message.png"
+
+export const rewards = [
+  {
+    title: "Earn daily rewards on your idle tokens",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nulla suspendisse tortor aene.",
+    bullets: [
+      {
+        icon: StockIcon,
+        text: "Lowest fees in market",
+      },
+      {
+        icon: LightningIcon,
+        text: "Fast and secure transactions",
+      },
+      {
+        icon: LockIcon,
+        text: "256-bit secure encryption",
+      },
+    ],
+  },
+  {
+    title: "Earn daily rewards on your idle tokens",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nulla suspendisse tortor aene.",
+    bullets: [
+      {
+        icon: CheckMarkIcon,
+        text: "100% Private data",
+      },
+      {
+        icon: UptimeIcon,
+        text: "99.99% Uptime guarantee",
+      },
+      {
+        icon: MessageIcon,
+        text: "24/7 Dedicated support",
+      },
+    ],
+  },
+]
