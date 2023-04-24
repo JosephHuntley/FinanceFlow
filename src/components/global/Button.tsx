a848ff98ac455f5e2057d612ddd521b9621d0223
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-24 19:46:54 -0400

    Changed font-weight to bold

--- src/components/global/Button.tsx
+++ src/components/global/Button.tsx
@@ -1,9 +1,9 @@
-import { JSX, Component } from "solid-js"
+import { JSX, Component } from "solid-js";
 
 const Button: Component<{
-  children: JSX.Element
-  variant?: "primary" | "alt" | "white"
-  className?: string
+  children: JSX.Element;
+  variant?: "primary" | "alt" | "white";
+  className?: string;
 }> = ({ children, variant, className = "" }) => {
   return (
     <button
@@ -15,11 +15,11 @@ const Button: Component<{
           : "bg-blue"
       } ${variant === "alt" ? "hover:bg-blue" : "hover:bg-alt-blue"} ${
         variant === "white" ? "text-blue hover:text-white" : ""
-      }  rounded-[5rem] py-5 px-8 flex flex-row justify-center items-center gap-1 w-52 h-14 leading-4 sm:min-w-[200px] w-full sm:w-min  ${className}`}
+      }  rounded-[5rem] py-5 px-8 flex flex-row justify-center items-center gap-1 w-52 h-14 leading-4 font-bold sm:min-w-[200px] w-full sm:w-min  ${className}`}
     >
       {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
