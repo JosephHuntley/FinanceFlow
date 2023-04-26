469ed7e31484121c2e0af079766fe0a96367cd5b
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-25 20:47:10 -0400

    Fixed overflow responsive

--- src/components/home/Rewards.tsx
+++ src/components/home/Rewards.tsx
@@ -5,7 +5,7 @@ import Laptop from "/Computer.png";
 
 const Rewards: Component<{}> = (props) => {
   return (
-    <section class="grid sm:gap-y-10  my-20 grid-cols-1 sm:grid-cols-2 items-center overflow-x-hidden mr-[-10rem]">
+    <section class="grid sm:gap-y-10  my-20 grid-cols-1 sm:grid-cols-2 items-center overflow-x-hidden sm:mr-[-10rem]">
       {/* Dual Phone */}
       <img class="w-full h-[380px] sm:w-[500px] sm:h-[592px]" src={DualPhone} />
       {rewards.map((reward) => (
@@ -23,9 +23,9 @@ const Rewards: Component<{}> = (props) => {
         </div>
       ))}
       {/* Laptop */}
-      <div class="w-full sm:w-[972px] sm:h-[588px] sm:right-[200px] relative mr-[-10rem]">
+      <div class="w-full sm:w-[972px] sm:h-[588px] sm:right-[200px] relative sm:mr-[-10rem]">
         <img
-          class="sm:relative w-full sm:w-[972px] sm:h-[588px] mr-[-10rem]"
+          class="sm:relative w-full sm:w-[972px] sm:h-[588px] sm:mr-[-10rem]"
           src={Laptop}
         />
       </div>
