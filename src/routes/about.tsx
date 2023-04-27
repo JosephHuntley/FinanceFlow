461b50a59e1e71ec7f36ee488b7a06e9d74cd3df
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-27 12:05:08 -0400

    Added SEO to about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,3 +1,4 @@
+import { Head, Title, Meta } from "solid-start";
 import Hero from "~/components/about/Hero";
 import Investor from "~/components/about/Investor";
 import Mission from "~/components/about/Mission";
@@ -6,12 +7,20 @@ import Timeline from "~/components/about/Timeline";
 
 export default function About() {
   return (
-    <main class="pt-32 sm:mx-40 mx-4">
-      <Hero />
-      <Mission />
-      <Timeline />
-      <Team />
-      <Investor />
-    </main>
+    <>
+      <Head>
+        <Title>About | FinanceFlow</Title>
+        <Meta charset="utf-8" />
+        <Meta name="description" content="About page of Finance Flow" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main class="pt-32 sm:mx-40 mx-4">
+        <Hero />
+        <Mission />
+        <Timeline />
+        <Team />
+        <Investor />
+      </main>
+    </>
   );
 }
