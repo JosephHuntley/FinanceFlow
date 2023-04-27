f59c3ca3f612c4de74a3c61d942a4eba6d8e00f6
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-27 12:05:31 -0400

    Added SEO and main to Pricing page

--- src/routes/pricing.tsx
+++ src/routes/pricing.tsx
@@ -1,8 +1,18 @@
 import { Component } from "solid-js";
+import { Head, Title, Meta } from "solid-start";
 
 const pricing: Component<{}> = (props) => {
-  
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <Title>Pricing | FinanceFlow</Title>
+        <Meta charset="utf-8" />
+        <Meta name="description" content="About page of Finance Flow" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main class="pt-32 sm:mx-40 mx-4"></main>
+    </>
+  );
 };
 
 export default pricing;
